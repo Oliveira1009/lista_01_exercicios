98f71d776ca4ba23bac54138158531be4af42757
Oliveira1009 <tamires.oliveira6@senacsp.edu.br>
2025-01-17 20:27:56 -0300

    Update Program.cs

--- lista_09/Program.cs
+++ lista_09/Program.cs
@@ -12,3 +12,46 @@ Exemplo de saída: "Domingo", "Segunda-feira", etc., ou "Número inválido. Digi
    */
 
 
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um número de 1 a 7 para saber o dia da semana: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        if (numero == 1)
+        {
+            Console.WriteLine("Domingo");
+        }
+        else if (numero == 2)
+        {
+            Console.WriteLine("Segunda-feira");
+        }
+        else if (numero == 3)
+        {
+            Console.WriteLine("Terça-feira");
+        }
+        else if (numero == 4)
+        {
+            Console.WriteLine("Quarta-feira");
+        }
+        else if (numero == 5)
+        {
+            Console.WriteLine("Quinta-feira");
+        }
+        else if (numero == 6)
+        {
+            Console.WriteLine("Sexta-feira");
+        }
+        else if (numero == 7)
+        {
+            Console.WriteLine("Sábado");
+        }
+        else
+        {
+            Console.WriteLine("Número inválido. Digite um número de 1 a 7.");
+        }
+    }
+}
