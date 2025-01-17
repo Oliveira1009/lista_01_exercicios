411c45c04a3c295f81c93d6a166f8b4556dad17b
Oliveira1009 <tamires.oliveira6@senacsp.edu.br>
2025-01-17 20:25:29 -0300

    Update Program.cs

--- lista_05/Program.cs
+++ lista_05/Program.cs
@@ -6,3 +6,23 @@ Objetivo: Aplicar condições para classificar os dados.
 Descrição: Desenvolva um programa que pergunte a idade de uma pessoa e determine se ela é maior de idade (18 anos ou mais) ou menor de idade. Use if e else para exibir a mensagem apropriada. 
 Exemplo de saída: "Você é maior de idade." ou "Você é menor de idade".
    */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite sua idade: ");
+        int idade = int.Parse(Console.ReadLine());
+
+        if (idade >= 18)
+        {
+            Console.WriteLine("Você é maior de idade.");
+        }
+        else
+        {
+            Console.WriteLine("Você é menor de idade.");
+        }
+    }
+}
