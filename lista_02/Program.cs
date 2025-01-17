0c64e83a69f371be972389bf6452fbb3c17e47da
Oliveira1009 <tamires.oliveira6@senacsp.edu.br>
2025-01-17 20:23:31 -0300

    Update Program.cs

--- lista_02/Program.cs
+++ lista_02/Program.cs
@@ -10,3 +10,25 @@ Descrição: Crie um programa que verifique se um número inserido pelo usuário
 Exemplo de saída: "O número está entre 1 e 100." ou "O número não está entre 1 e 100."
 
 */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Solicita ao usuário que digite um número
+        Console.Write("Digite um número: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        // Verifica se o número está no intervalo de 1 a 100
+        if (numero >= 1 && numero <= 100)
+        {
+            Console.WriteLine("O número está entre 1 e 100.");
+        }
+        else
+        {
+            Console.WriteLine("O número não está entre 1 e 100.");
+        }
+    }
+}
