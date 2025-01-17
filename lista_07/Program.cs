2322aea930ebd7955c82f3e5d1f152ed64520cfc
Oliveira1009 <tamires.oliveira6@senacsp.edu.br>
2025-01-17 20:26:42 -0300

    Update Program.cs

--- lista_07/Program.cs
+++ lista_07/Program.cs
@@ -6,3 +6,30 @@ Objetivo: Pratique a comparação de variáveis.
 Descrição: Escreva um programa que solicite ao usuário dois números e determine qual é o maior. Use if, else if e else para exibir a mensagem apropriada. 
 Exemplo de saída: "O primeiro número é maior.", "O segundo número é maior." ou "Os números são iguais."
    */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite o primeiro número: ");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o segundo número: ");
+        int num2 = int.Parse(Console.ReadLine());
+
+        if (num1 > num2)
+        {
+            Console.WriteLine("O primeiro número é maior.");
+        }
+        else if (num2 > num1)
+        {
+            Console.WriteLine("O segundo número é maior.");
+        }
+        else
+        {
+            Console.WriteLine("Os números são iguais.");
+        }
+    }
+}
